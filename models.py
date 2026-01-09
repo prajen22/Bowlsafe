35c6a3c60ccc3fb2d2c1578743d1d42f53495d76
Prajen Sk <123826741+prajen22@users.noreply.github.com>
2026-01-10 01:21:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,13 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey
-from app.database import Base
+from sqlalchemy import Column, Integer, String, Date
+from database import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
 
 
 class Progress(Base):
